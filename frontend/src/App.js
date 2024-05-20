53283c20c2a1cf203c6209f1ab5bc964dc9a85a2
Emma Paris <eparis@atlassian.com>
2024-05-20 18:11:51 +0200

    navbar

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import SiteNavbar from './components/navbar/SiteNavbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+    <SiteNavbar></SiteNavbar>
+    </>
+  );
+}
+
+export default App;
