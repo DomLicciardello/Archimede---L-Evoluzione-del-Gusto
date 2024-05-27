05463d47bbcffc1a7b4c0dbf4f49249198076797
Emma Paris <eparis@atlassian.com>
2024-05-27 23:08:25 +0200

    aggiunta footer

--- frontend/src/components/home/SiteHomeFooter.jsx
+++ frontend/src/components/home/SiteHomeFooter.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
 import './style.css'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function SiteHomeFooter() {
   return (
-    <div
-    className='footer_style'>
-        <h2>spazio footer</h2>
-    </div>
+    <Container fluid className='footer_style'>
+    <Row>
+      <Col sm={12} md={4} lg={4}
+      className='footer_div_style'>
+        <p style={{fontSize:'16px', marginBottom:'10px'}}><ion-icon name="time-outline" style={{marginRight:'5px', fontSize:'22px'}}></ion-icon>orario</p>
+        <p>lun: chiuso</p>
+        <p>mar-sab: 9-20</p>
+        <p>dom: 9-13</p>
+      </Col>
+      <Col sm={12} md={4} lg={4}
+      className='footer_div_style'>
+        <p style={{fontSize:'16px', marginBottom:'10px'}}><ion-icon name="location-outline" style={{marginRight:'5px', fontSize:'22px'}}></ion-icon>dove siamo</p>
+        <p>via maffacini, 3</p>
+        <p>43036, Fidenza PR</p>
+      </Col>
+      <Col sm={12} md={4} lg={4}
+      className='footer_div_style'
+      style={{flexDirection:'column'}}>
+        <p style={{fontSize:'16px', marginBottom:'10px'}}>social</p>
+        <div>
+        <ion-icon name="logo-facebook"style={{marginRight:'10px'}}></ion-icon>
+        <ion-icon name="logo-instagram"style={{marginRight:'10px'}}></ion-icon>
+        <ion-icon name="logo-whatsapp"></ion-icon>
+        </div>
+        <p style={{fontSize:'13px'}}>privacy policy</p>
+      </Col>
+      <Col>
+      <p style={{margin:'0px', fontSize:'13px'}}>2024 © ARCHIMEDE DI L&D S.R.L.</p>
+      </Col>
+    </Row>
+  </Container>
   )
 }
--- frontend/src/components/home/SiteHomeInstagram.jsx
+++ frontend/src/components/home/SiteHomeInstagram.jsx
@@ -24,7 +24,7 @@ export default function SiteHomeInstagram() {
       </Row>
       <Row>
         <Col
-        md={12} lg={4}
+        md={12} lg={6}
         className='d-flex justify-content-center align-items-center pe-0 mt-1 mb-5'>
         <div style={{width:'300px', height:'584px', backgroundImage: `url(${smartphone})`, backgroundSize:"cover", position:'relative'}}>
           <div className='insta_scroll'>
@@ -38,17 +38,17 @@ export default function SiteHomeInstagram() {
         </div>
         </Col>
         <Col
-        md={12} lg={8}
-        className='d-flex justify-content-center align-items-center'>
-        <div className='d-flex flex-column justify-content-center align-items-center'>
-          <p style={{fontSize:'20px', textAlign:'center', marginInline:'5px'}}>MANGIA, SCATTA E TAGGA @ARCHIMEDEIT SULLE TUE STORIE INSTAGRAM!</p>
-          <p style={{fontSize:'20px', textAlign:'center', marginInline:'5px'}}>IN QUESTO MODO PARTECIPERAI OGNI MESE AD UN ESTRAZIONE PER VINCERE UN REGALO!</p>
+        md={12} lg={6}
+        className='d-flex flex-column justify-content-center align-items-center p-0'>
+        <div className='d-flex flex-column justify-content-center align-items-center ms-5 me-5'>
+          <p style={{fontSize:'20px', textAlign:'center'}}>MANGIA, SCATTA E TAGGA @ARCHIMEDEIT SULLE TUE STORIE INSTAGRAM!</p>
+          <p style={{fontSize:'20px', textAlign:'center'}}>IN QUESTO MODO PARTECIPERAI OGNI MESE AD UN ESTRAZIONE PER VINCERE UN REGALO!</p>
           <button className='button_instagram d-flex align-items-center'><ion-icon name="logo-instagram" style={{marginRight:'5px'}}></ion-icon>seguici su instagram</button>
           <div className='riga_titolo'><span></span></div>
-          <p style={{fontSize:'20px', textAlign:'center', marginInline:'5px'}}>METTI "MI PIACE" ALLA NOSTRA PAGINA FACEBOOK PER RESTARE AGGIORNATO SU TUTTE LE NOVITÀ!</p>
+          <p style={{fontSize:'20px', textAlign:'center'}}>METTI "MI PIACE" ALLA NOSTRA PAGINA FACEBOOK PER RESTARE AGGIORNATO SU TUTTE LE NOVITÀ!</p>
           <button className='button_facebook d-flex align-items-center'><ion-icon name="logo-facebook" style={{marginRight:'5px'}}></ion-icon>seguici su facebook</button>
           <div className='riga_titolo'><span></span></div>
-          <p style={{fontSize:'20px', textAlign:'center', marginInline:'5px'}}>PRENOTA ARANCINI E CANNOLI D'ASPORTO CON IL NOSTRO CATALOGO WHATSAPP!</p>
+          <p style={{fontSize:'20px', textAlign:'center'}}>PRENOTA ARANCINI E CANNOLI D'ASPORTO CON IL NOSTRO CATALOGO WHATSAPP!</p>
           <button className='button_whatsapp d-flex align-items-center'><ion-icon name="logo-whatsapp" style={{marginRight:'5px'}}></ion-icon>catalogo whatsapp</button>
           <div className='riga_titolo'><span></span></div>
         </div>
